9ac6d39e02083e2b90e71d54cd4be75227daff0e
Cesar Oyarzun <cesar.oyarzun@scala.com>
2016-01-05 15:08:12 -0500

    EXP-1228 EXP-1229 add uuid constant

--- exp-android-sdk/src/main/java/com/scala/exp/android/sdk/model/Location.java
+++ exp-android-sdk/src/main/java/com/scala/exp/android/sdk/model/Location.java
@@ -1,6 +1,7 @@
 package com.scala.exp.android.sdk.model;
 
 import com.scala.exp.android.sdk.AppSingleton;
+import com.scala.exp.android.sdk.Utils;
 
 /**
  * Created by Cesar Oyarzun on 10/28/15.
@@ -9,7 +10,7 @@ import com.scala.exp.android.sdk.AppSingleton;
 public class Location extends AbstractModel {
 
     public String getLayoutUrl(){
-        return  AppSingleton.getInstance().getHost()+"/api/locations/"+getString("uuid")+"/layout";
+        return  AppSingleton.getInstance().getHost()+"/api/locations/"+getString(Utils.UUID)+"/layout";
     }
 
 }
