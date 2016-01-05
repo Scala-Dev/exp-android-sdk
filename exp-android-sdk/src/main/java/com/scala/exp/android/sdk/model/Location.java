b1819c935c09b921418432c464636a1d78c46498
Cesar Oyarzun <cesar.oyarzun@scala.com>
2016-01-05 15:05:43 -0500

    EXP-1228 EXP-1229 add getLayoutUrl to location

--- exp-android-sdk/src/main/java/com/scala/exp/android/sdk/model/Location.java
+++ exp-android-sdk/src/main/java/com/scala/exp/android/sdk/model/Location.java
@@ -1,9 +1,15 @@
 package com.scala.exp.android.sdk.model;
 
+import com.scala.exp.android.sdk.AppSingleton;
+
 /**
  * Created by Cesar Oyarzun on 10/28/15.
  */
 
 public class Location extends AbstractModel {
+
+    public String getLayoutUrl(){
+        return  AppSingleton.getInstance().getHost()+"/api/locations/"+getString("uuid")+"/layout";
+    }
 
 }
