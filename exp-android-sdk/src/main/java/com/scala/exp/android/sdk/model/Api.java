30acc5f039b6fab7abef5100985c6d2d27855144
Cesar Oyarzun <cesar.oyarzun@scala.com>
2017-01-06 17:05:43 -0500

    EXP-3671 EXP-3671 update Auth object

---
+++ exp-android-sdk/src/main/java/com/scala/exp/android/sdk/model/Api.java
@@ -0,0 +1,15 @@
+package com.scala.exp.android.sdk.model;
+
+/**
+ * Created by Cesar Oyarzun on 1/6/17.
+ */
+public class Api extends AbstractModel{
+    private String host;
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
